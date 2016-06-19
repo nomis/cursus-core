060f35ecfbbc1ce10858f7368a25e926acf8348a
Simon Arlott <sa.me.uk>
2016-06-19 12:27:38 +0100

    add CC 2016 race 5

--- src/test/java/org/spka/cursus/test/cc_2016/CCSeries2016.java
+++ src/test/java/org/spka/cursus/test/cc_2016/CCSeries2016.java
@@ -27,9 +27,10 @@ import org.spka.cursus.test.AbstractSPKASeries;
 
 import eu.lp0.cursus.db.data.Penalty;
 
+@SuppressWarnings("nls")
 public class CCSeries2016 extends AbstractSPKASeries {
 	public CCSeries2016(boolean top3) {
-		super("Celtic Challenge 2016", top3 ? CCConstants.UUID_2013 : FISLYConstants.UUID_2010, "Scotland", "Ireland"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		super("Celtic Challenge 2016", top3 ? CCConstants.UUID_2013 : FISLYConstants.UUID_2010, "Scotland", "Ireland");
 	}
 
 	protected static final int SERIES_FLEET = 9;
@@ -46,55 +47,62 @@ public class CCSeries2016 extends AbstractSPKASeries {
 		super.createDatabase();
 
 		addSeries();
-		addPilot("SCO116@2010", MALE, "Scotland"); //$NON-NLS-1$ //$NON-NLS-2$
-		addPilot("SCO159@2005", MALE, "Scotland"); //$NON-NLS-1$ //$NON-NLS-2$
-		addPilot("SCO808@2010", MALE, "Scotland"); //$NON-NLS-1$ //$NON-NLS-2$
-		addPilot("IR016@2010", MALE, "Ireland"); //$NON-NLS-1$ //$NON-NLS-2$
-		addPilot("IR021@2016", MALE, "Ireland"); //$NON-NLS-1$ //$NON-NLS-2$
-		addPilot("IR023@2016", MALE, "Ireland"); //$NON-NLS-1$ //$NON-NLS-2$
-		addPilot("IR053@2010", MALE, "Ireland"); //$NON-NLS-1$ //$NON-NLS-2$
-		addPilot("IR077@2009", MALE, "Ireland"); //$NON-NLS-1$ //$NON-NLS-2$
-		addPilot("IR085@2008", MALE, "Ireland"); //$NON-NLS-1$ //$NON-NLS-2$
+		addPilot("SCO116@2010", MALE, "Scotland");
+		addPilot("SCO159@2005", MALE, "Scotland");
+		addPilot("SCO808@2010", MALE, "Scotland");
+		addPilot("IR016@2010", MALE, "Ireland");
+		addPilot("IR021@2016", MALE, "Ireland");
+		addPilot("IR023@2016", MALE, "Ireland");
+		addPilot("IR053@2010", MALE, "Ireland");
+		addPilot("IR077@2009", MALE, "Ireland");
+		addPilot("IR085@2008", MALE, "Ireland");
 	}
 
 	protected void createEvent1Races() throws Exception {
-		addEvent(1, "Benone Strand (18/06/2016 and 19/06/2016)"); //$NON-NLS-1$
+		addEvent(1, "Benone Strand (18/06/2016 and 19/06/2016)");
 
-		addRace(1, 1, "Benone Strand (18/06/2016)"); //$NON-NLS-1$
-		addAttendees(1, 1, PILOT, "SCO116", "SCO159", "SCO808"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		addAttendees(1, 1, PILOT, "IR016", "IR021", "IR023", "IR053", "IR077", "IR085"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
-		addLaps(1, 1, "116,808,21,53,77,85,159,23"); //$NON-NLS-1$
-		addLaps(1, 1, "116,21,808,53,85,159,77,23"); //$NON-NLS-1$
-		addLaps(1, 1, "116,21,808,53,85,159,77,23"); //$NON-NLS-1$
-		addLaps(1, 1, "116,21,808,53,85,159,77,23"); //$NON-NLS-1$
-		addLaps(1, 1, "116,21,808,53,85,159,77"); //$NON-NLS-1$
-		addLaps(1, 1, "116,21,808,53,85,159"); //$NON-NLS-1$
-		addLaps(1, 1, "116,21,808,53"); //$NON-NLS-1$
+		addRace(1, 1, "Benone Strand (18/06/2016)");
+		addAttendees(1, 1, PILOT, "SCO116", "SCO159", "SCO808");
+		addAttendees(1, 1, PILOT, "IR016", "IR021", "IR023", "IR053", "IR077", "IR085");
+		addLaps(1, 1, "116,808,21,53,77,85,159,23");
+		addLaps(1, 1, "116,21,808,53,85,159,77,23");
+		addLaps(1, 1, "116,21,808,53,85,159,77,23");
+		addLaps(1, 1, "116,21,808,53,85,159,77,23");
+		addLaps(1, 1, "116,21,808,53,85,159,77");
+		addLaps(1, 1, "116,21,808,53,85,159");
+		addLaps(1, 1, "116,21,808,53");
 
-		addRace(1, 2, "Benone Strand (18/06/2016)"); //$NON-NLS-1$
-		addAttendees(1, 2, PILOT, "SCO116", "SCO159", "SCO808"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		addAttendees(1, 2, PILOT, "IR016", "IR021", "IR023", "IR053", "IR077", "IR085"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
-		addLaps(1, 2, "21,116,53,77,159,808,23"); //$NON-NLS-1$
-		addLaps(1, 2, "21,116,53,77,159"); //$NON-NLS-1$
-		addLaps(1, 2, "21,116,53,77,159"); //$NON-NLS-1$
-		addLaps(1, 2, "21,116,53"); //$NON-NLS-1$
+		addRace(1, 2, "Benone Strand (18/06/2016)");
+		addAttendees(1, 2, PILOT, "SCO116", "SCO159", "SCO808");
+		addAttendees(1, 2, PILOT, "IR016", "IR021", "IR023", "IR053", "IR077", "IR085");
+		addLaps(1, 2, "21,116,53,77,159,808,23");
+		addLaps(1, 2, "21,116,53,77,159");
+		addLaps(1, 2, "21,116,53,77,159");
+		addLaps(1, 2, "21,116,53");
 
-		addRace(1, 3, "Benone Strand (18/06/2016)"); //$NON-NLS-1$
-		addAttendees(1, 3, PILOT, "SCO116", "SCO159", "SCO808"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		addAttendees(1, 3, PILOT, "IR016", "IR021", "IR023", "IR053", "IR077", "IR085"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
-		addLaps(1, 3, "21,808,116,53,159,77,85,16"); //$NON-NLS-1$
-		addLaps(1, 3, "21,116,808,53,159,77,85"); //$NON-NLS-1$
-		addLaps(1, 3, "21,116,53,808,159,77"); //$NON-NLS-1$
-		addLaps(1, 3, "21,116,53,808,159,77"); //$NON-NLS-1$
-		addLaps(1, 3, "808,116,159,53,21,77"); //$NON-NLS-1$
-		addPenalty(1, 3, "SCO808", new Penalty(AUTOMATIC, "Hit scoring line mark")); //$NON-NLS-1$ //$NON-NLS-2$
+		addRace(1, 3, "Benone Strand (18/06/2016)");
+		addAttendees(1, 3, PILOT, "SCO116", "SCO159", "SCO808");
+		addAttendees(1, 3, PILOT, "IR016", "IR021", "IR023", "IR053", "IR077", "IR085");
+		addLaps(1, 3, "21,808,116,53,159,77,85,16");
+		addLaps(1, 3, "21,116,808,53,159,77,85");
+		addLaps(1, 3, "21,116,53,808,159,77");
+		addLaps(1, 3, "21,116,53,808,159,77");
+		addLaps(1, 3, "808,116,159,53,21,77");
+		addPenalty(1, 3, "SCO808", new Penalty(AUTOMATIC, "Hit scoring line mark"));
 
-		addRace(1, 4, "Benone Strand (18/06/2016)"); //$NON-NLS-1$
-		addAttendees(1, 4, PILOT, "SCO116", "SCO159", "SCO808"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		addAttendees(1, 4, PILOT, "IR016", "IR021", "IR023", "IR053", "IR077", "IR085"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
-		addLaps(1, 4, "116,53,159,77,808,21,23"); //$NON-NLS-1$
-		addLaps(1, 4, "116,53,159,808,21,77"); //$NON-NLS-1$
-		addLaps(1, 4, "116,53,159,808,77,21"); //$NON-NLS-1$
-		addLaps(1, 4, "53,116,808,159"); //$NON-NLS-1$
+		addRace(1, 4, "Benone Strand (18/06/2016)");
+		addAttendees(1, 4, PILOT, "SCO116", "SCO159", "SCO808");
+		addAttendees(1, 4, PILOT, "IR016", "IR021", "IR023", "IR053", "IR077", "IR085");
+		addLaps(1, 4, "116,53,159,77,808,21,23");
+		addLaps(1, 4, "116,53,159,808,21,77");
+		addLaps(1, 4, "116,53,159,808,77,21");
+		addLaps(1, 4, "53,116,808,159");
+
+		addRace(1, 5, "Benone Strand (19/06/2016)");
+		addAttendees(1, 5, PILOT, "SCO116", "SCO159", "SCO808");
+		addAttendees(1, 5, PILOT, "IR021", "IR023", "IR053", "IR077", "IR085");
+		addLaps(1, 5, "21,77,116,53,808,159");
+		addLaps(1, 5, "21,116,77");
+		addPenalty(1, 5, "IR021", new Penalty(AUTOMATIC, "Did not start at the back of the fleet"));
 	}
 }

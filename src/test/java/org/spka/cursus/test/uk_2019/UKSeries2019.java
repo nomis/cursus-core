5d30fedaac6114d008eae80aa18237575a90af8d
Simon Arlott <sa.me.uk>
2019-09-05 15:05:55 +0100

    add 4 nations 2019 races 2 and 3

--- src/test/java/org/spka/cursus/test/uk_2019/UKSeries2019.java
+++ src/test/java/org/spka/cursus/test/uk_2019/UKSeries2019.java
@@ -104,13 +104,13 @@ public class UKSeries2019 extends AbstractSPKASeries {
 		super.createDatabase();
 
 		addSeries();
-		addPilot("SCO023@2005", MALE, "Scotland", "DP", "DP1", "DP2", "DP3", "DP4");
 		addPilot("SCO045@2016", MALE, "Scotland", "DP", "DP1", "DP2", "DP3", "DP4");
 		addPilot("SCO066@2013", MALE, "Scotland", "DP", "DP1", "DP2", "DP3", "DP4");
 		addPilot("SCO116@2010", MALE, "Scotland", "DP", "DP1", "DP2", "DP3", "DP4");
-		addPilot("SCO156@2010", MALE, "Scotland");
+		addPilot("SCO156@2010", MALE, "Scotland", "FB", "FB1", "FB2", "FB3", "FB4");
 		addPilot("SCO179@2005", MALE, "Scotland", "DP", "DP1", "DP2", "DP3", "DP4");
 		addPilot("SCO808@2010", MALE, "Scotland", "DP", "DP1", "DP2", "DP3", "DP4");
+		addPilot("K020@2019", MALE, "Scotland", "DP", "DP1", "DP2", "DP3", "DP4");
 
 		addPilot("K040@2017", MALE, "England", "DP", "DP1", "DP2", "DP3", "DP4");
 		addPilot("K053@2017", FEMALE, "England", "DP", "DP1", "DP2", "DP3", "DP4");
@@ -122,26 +122,41 @@ public class UKSeries2019 extends AbstractSPKASeries {
 		addPilot("K705@2017", MALE, "England", "DP", "DP1", "DP2", "DP3", "DP4");
 		addPilot("K854@2015", MALE, "England", "DP", "DP1", "DP2", "DP3", "DP4");
 		addPilot("K862@2019", FEMALE, "England", "DP", "DP1", "DP2", "DP3", "DP4");
-		addPilot("K866@2019", MALE, "England");
+		addPilot("K866@2019", MALE, "England", "DP", "DP1", "DP2", "DP3", "DP4");
 		addPilot("SW1148@2019", MALE, "England", "FB", "FB1", "FB2", "FB3", "FB4");
 
 		addAlias("SCO066", "SCO004");
 		addAlias("SCO116", "SCO001");
 		addAlias("SCO179", "SCO003");
 		addAlias("SCO808", "SCO002");
-		addAlias("SCO023", "SCO011");
+		addAlias("K020", "SCO011");
 	}
 
 	protected void createEvent1Races() throws Exception {
 		addEvent(1, "Luce Bay (05/09/2019)");
 
 		addRace(1, 1, "Luce Bay (05/09/2019)");
-		addAttendees(1, 1, PILOT, "SCO023", "SCO045", "SCO066", "SCO116", "SCO156", "SCO179", "SCO808");
+		addAttendees(1, 1, PILOT, "SCO045", "SCO066", "SCO116", "SCO156", "SCO179", "SCO808", "K020");
 		addAttendees(1, 1, PILOT, "K040", "K053", "K060", "K101", "K169", "K176", "K705", "K854", "K866", "SW1148");
+		addPenalty(1, 1, "K176", new Penalty(Type.AUTOMATIC, "Hit mark 2"));
 		addPenalty(1, 1, "K176", new Penalty(Type.AUTOMATIC, "Hit mark 8"));
 		addLaps(1, 1, "101,1,854,11,866,705,4,40,3,60,2,45,53");
 		addLaps(1, 1, "101,854,1,11,705,866,40,3,176,4,2,53,60,156,45");
 		addLaps(1, 1, "101,854,1,11,705,866,40,3,2,4,53,60,156");
+
+		addRace(1, 2, "Luce Bay (05/09/2019)");
+		addAttendees(1, 2, PILOT, "SCO045", "SCO066", "SCO116", "SCO156", "SCO179", "SCO808", "K020");
+		addAttendees(1, 2, PILOT, "K040", "K053", "K060", "K101", "K169", "K176", "K705", "K854", "K866", "SW1148");
+		addLaps(1, 2, "101,1,866,854,11,2,3,53,40,4,60,45");
+		addLaps(1, 2, "101,156,866,176,11,40,854,3,4,53");
+		addLaps(1, 2, "101,866,11,40,854,3,4,60,45,176,156");
+
+		addRace(1, 3, "Luce Bay (05/09/2019)");
+		addAttendees(1, 3, PILOT, "SCO045", "SCO066", "SCO116", "SCO156", "SCO179", "SCO808", "K020");
+		addAttendees(1, 3, PILOT, "K040", "K053", "K060", "K101", "K169", "K176", "K705", "K854", "K866", "SW1148");
+		addLaps(1, 3, "101,1,3,40,11,854,53,866,4,60,156");
+		addLaps(1, 3, "101,176,45,1,40,3,11,854,866,2,53,4,60");
+		addLaps(1, 3, "101,1,40,854,11,3,866,53,156,4,176,2,45,60");
 	}
 
 	protected void createEvent2Races() throws Exception {
